636adbed69339dfd978419694aab459ddd1a916a
Ilie Daniel Rus <rus.ilie.daniel@gmail.com>
2023-10-26 23:16:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "symboltable.cpp" 
-#include <cassert> 
+#include "symboltable.cpp"
+#include <cassert>
 #include <iostream>
 
 int main() {
@@ -8,17 +8,17 @@ int main() {
     symbolTable.add("1");
     assert(symbolTable.containsTerm("1"));
     std::pair<int, int> position1 = symbolTable.findPositionOfTerm("1");
-    assert(position1.first == 1 && position1.second == 0);
+    assert(position1.first == 4 && position1.second == 0);
 
     symbolTable.add("6");
     assert(symbolTable.containsTerm("6"));
     std::pair<int, int> position6 = symbolTable.findPositionOfTerm("6");
-    assert(position6.first == 1 && position6.second == 1);
+    assert(position6.first == 4 && position6.second == 1);
 
     symbolTable.add("5");
     assert(symbolTable.containsTerm("5"));
     std::pair<int, int> position5 = symbolTable.findPositionOfTerm("5");
-    assert(position5.first == 2 && position5.second == 0);
+    assert(position5.first == 3 && position5.second == 0);
 
     std::string hashTableString = symbolTable.getHashTable().toString();
 
